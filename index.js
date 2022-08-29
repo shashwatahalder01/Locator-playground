cc0ecfed6869256f06687c0a29823463c4b83869
shashwata <shashwatahalder01@gmail.com>
2022-08-30 00:47:07 +0600

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function sweetalert() {
+  // Swal.fire(
+  //   'Good job!',
+  //   'You clicked the button!',
+  //   'success'
+  // )
+
+  Swal.fire({
+    icon: 'success',
+    title: 'Good job!',
+    text: 'You clicked the button!',
+    // confirmButtonText: 'Ok',
+  })
+}
